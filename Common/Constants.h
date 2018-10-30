99d6b267e13b6a1c1f297eaf48df566c56a7a5bc
Nick Reffitt <nickreffitt@gmail.com>
2018-10-30 12:56:54 +0000

    Add Constants.h

---
+++ Common/Constants.h
@@ -0,0 +1,11 @@
+//
+//  Constants.h
+//
+//  Created by Nick Reffitt on 15/12/2016.
+//  Copyright © 2016 Tapdaq. All rights reserved.
+//
+
+@class NSString;
+
+static NSString *const kAppId = <YOUR_APP_ID>;
+static NSString *const kClientKey = <YOUR_CLIENT_KEY>;
